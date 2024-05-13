ef09d82fb4d4684b631b87e044316b1291c624bf
cristianxiix <131236749+cristianxiix@users.noreply.github.com>
2024-05-13 12:20:45 +0300

    Update Classes.cpp

--- Event Handler/Classes.cpp
+++ Event Handler/Classes.cpp
@@ -101,33 +101,5 @@ void DeleteAgenda(Agenda*& a)
 		return;
 }
 
-Event* createEvent()
-{
-
-	std::string tempN;
-	std::string tempLoc;
-	DateTime* dt = new DateTime();
-	//std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-
-	std::cout << "What name should i assign to your event?\n";
-	std::cin >> tempN;
-	std::cin.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-	std::cout << "What location should i assign to your event?(string)\n";
-	std::getline(std::cin, tempLoc);
-	std::cout << "What date time should i assign to your event?\nInsert day, month, year one after the other:\n";
-	std::cout << "day: \n";
-	std::cin >> dt->day;
-	std::cout << "month: \n";
-	std::cin >> dt->month;
-	std::cout << "year: \n";
-	std::cin >> dt->year;
-
-	Event* e = new Event(dt, tempLoc, tempN);
-	std::cout << "Event successfully created!" << std::endl;
-	return e;
-}
-
-
-
 
 
